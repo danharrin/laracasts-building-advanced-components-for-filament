da569cacdd25c502bcdc632657c6732c800a809a
Dan Harrin <git@danharrin.com>
2023-08-07 19:01:49 +0100

    Make TextInput macroable

--- app/Components/TextInput.php
+++ app/Components/TextInput.php
@@ -5,10 +5,13 @@ namespace App\Components;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Macroable;
 use Livewire\Component;
 
 class TextInput implements Htmlable
 {
+    use Macroable;
+
     protected string | \Closure $label;
 
     protected int | \Closure | null $maxLength = null;
